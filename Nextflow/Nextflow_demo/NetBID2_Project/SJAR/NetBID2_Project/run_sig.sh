b9b5025a789e0852cc730a4cb93826b6980470e1
vasileioubill95 <vasileioubill95@gmail.com>
2026-06-12 14:52:02 +0300

    Update

--- Nextflow/Nextflow_demo/NetBID2_Project/SJAR/NetBID2_Project/run_sig.sh
+++
@@ -1,1 +0,0 @@
-sjaracne NetBID2_Project_SIG ..//NetBID2_Project/SJAR//NetBID2_Project/input.exp ..//NetBID2_Project/SJAR//NetBID2_Project/sig.txt ..//NetBID2_Project/SJAR/ 
--- Nextflow/Nextflow_demo/NetBID2_Project/SJAR/NetBID2_Project/run_tf.sh
+++
@@ -1,1 +0,0 @@
-sjaracne NetBID2_Project_TF ..//NetBID2_Project/SJAR//NetBID2_Project/input.exp ..//NetBID2_Project/SJAR//NetBID2_Project/tf.txt ..//NetBID2_Project/SJAR/ 
